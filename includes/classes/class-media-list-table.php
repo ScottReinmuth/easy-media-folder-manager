963dee0e1863e252c2ba8f5c98fdddec912b2a40
Scott Reinmuth <3734666+ScottReinmuth@users.noreply.github.com>
2025-07-10 22:42:06 -0600

    Update class-media-list-table.php

--- includes/classes/class-media-list-table.php
+++ includes/classes/class-media-list-table.php
@@ -14,6 +14,13 @@ if (!defined('ABSPATH')) {
  */
 class EMFM_Media_List_Table {
     /**
+     * Cached folders.
+     *
+     * @var WP_Term[]
+     */
+    private $folders = null;
+
+    /**
      * Initialize hooks.
      */
     public function init() {
@@ -28,7 +35,7 @@ class EMFM_Media_List_Table {
      * @return array Modified columns.
      */
     public function add_folder_column($columns) {
-        $columns['emfm_folder'] = __('Folder', 'easy-media-folder-manager');
+        $columns['emfm_media_folder'] = __('Folder', 'easy-media-folder-manager');
         return $columns;
     }
 
@@ -39,25 +46,28 @@ class EMFM_Media_List_Table {
      * @param int    $media_id    Media ID.
      */
     public function render_folder_column($column_name, $media_id) {
-        if ('emfm_folder' !== $column_name) {
+        if ('emfm_media_folder' !== $column_name) {
             return;
         }
 
         $core = new Easy_Media_Folder_Manager();
-        $folders = $core->get_folders();
+        if (is_null($this->folders)) {
+            $this->folders = $core->get_folders();
+        }
+
         $current_folders = wp_get_object_terms($media_id, 'emfm_media_folder', ['fields' => 'ids']);
         $current_folder_id = !empty($current_folders) ? absint($current_folders[0]) : 0;
 
         ?>
-        <select class="emfm-folder-select" data-media-id="<?php echo esc_attr($media_id); ?>">
+        <select class="emfm-folder-select" data-media-id="<?php echo esc_attr($media_id); ?>" aria-label="<?php esc_attr_e('Select folder for media item', 'easy-media-folder-manager'); ?>">
             <option value="0" <?php selected($current_folder_id, 0); ?>><?php esc_html_e('None', 'easy-media-folder-manager'); ?></option>
-            <?php foreach ($folders as $folder) : ?>
+            <?php foreach ($this->folders as $folder) : ?>
+                <?php $indent = $folder->parent ? str_repeat('&nbsp;&nbsp;', $core->get_term_depth($folder)) : ''; ?>
                 <option value="<?php echo esc_attr($folder->term_id); ?>" <?php selected($current_folder_id, $folder->term_id); ?>>
-                    <?php echo esc_html($folder->name); ?>
+                    <?php echo $indent . esc_html($folder->name); ?>
                 </option>
             <?php endforeach; ?>
         </select>
         <?php
     }
 }
-?>
